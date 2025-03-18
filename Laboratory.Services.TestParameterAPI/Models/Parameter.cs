b2bccfae7d6258e065ed0c6d92747fa3fe60f9fc
pk <kpmprabath@gmail.com>
2025-03-19 01:11:08 +1030

    Adding Parameters

--- Laboratory.Services.TestParameterAPI/Models/Parameter.cs
+++ Laboratory.Services.TestParameterAPI/Models/Parameter.cs
@@ -16,12 +16,14 @@ namespace Laboratory.Services.TestParameterAPI.Models
         public int? LabNumber { get; set; }
         
         public string? PatientName { get; set; }
+        //lipid
         public int? TotalCholesterol { get; set; }
         public int? Triglycerides { get; set; }
         public int? HDL { get; set; }
         public int? LDL { get; set; }
         public int? CholHDLRatio { get; set; }
         public int? NonHDLChol { get; set; }
+        //LFT
         public int? ALP { get; set; }
         public int? GGT { get; set; }
         public int? LD { get; set; }
@@ -33,6 +35,7 @@ namespace Laboratory.Services.TestParameterAPI.Models
         public int? Sodium { get; set; }
         public int? Potassium { get; set; }
         public int? Glucose { get; set; }
+        //FBC
         public int? WBC { get; set; }
         public int? Neutrophils { get; set; }
         public int? Lymphocytes { get; set; }
@@ -44,6 +47,26 @@ namespace Laboratory.Services.TestParameterAPI.Models
         public int? MCH { get; set; }
         public int? PlateletCount { get; set; }
         public int? MPV { get; set; }
+        //UFR
+        public string? Color { get; set; }
+        public int? SpecificGravity { get; set; }
+        public int? Appearance { get; set; }
+        public int? PH { get; set; }
+        public int? Protien { get; set; }
+        public int? Urobilinogen { get; set; }
+        public int? Ketones { get; set; }
+        public int? BileSalt { get; set; }
+        public int? Bilirubin { get; set; }
+        public int? Nitrite { get; set; }
+        public int? PusCells { get; set; }
+        public int? RedBloodCells { get; set; }
+        public int? EpithelialCells { get; set; }
+        public int? Casts { get; set; }
+        public int? Crystals { get; set; }
+        public int? Bacteria { get; set; }
+       
+
+
         
 
 
